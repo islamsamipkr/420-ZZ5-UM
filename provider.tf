87eddaa53133404b254ac0c73e547c5e7188c94e
Sami Islam <islam.samipkr@gmail.com>
2023-12-15 17:42:06 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,7 +12,7 @@ provider "azurerm"{
   features{}
   skip_provider_registration="true"
   
-  subscription_id=var.subscription_id
+  /*subscription_id=var.subscription_id*/
   client_id=var.client_id
   client_secret=var.client_secret
   tenant_id=var.tenant_id
