3e549f0df58908e84c852801cefe6a47f9307b65
Sami Islam <islam.samipkr@gmail.com>
2023-12-15 16:03:35 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "mcit420zz5um" {
-  name     = "montreal_college_resource_group_420-ZZ5-UM"
+  name     = "mcit_resource_group_420-ZZ5-UM"
   location = "canadacentral"
 }
