45d3a6a14edc83c14cec69aba4b0190d722c5608
Sami Islam <islam.samipkr@gmail.com>
2023-12-18 20:18:40 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -7,10 +7,10 @@ resource "azurerm_resource_group" "mcit420zz5um2" {
   name     = "mcit_resource_group_lecture2"
   location = "canadacentral"
 }
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "azurermmcit" {
   name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.mcit420zz5um.name
+  location                 = azurerm_resource_group.mcit420zz5um.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
