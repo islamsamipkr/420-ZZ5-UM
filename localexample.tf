9ef039abd31b343cc7eea8c2184260a51060cfc3
Sami Islam <islam.samipkr@gmail.com>
2023-12-20 19:49:33 -0500

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -3,13 +3,3 @@ locals {
   owner        = "Community Team"
   lengthofService_name=length(local.service_name)
 }
-output "print"{
-      value = local.service_name
-}
-output "secondprint"{
-      value = local.owner
-}
-
-output "thirdprint"{
-      value=local.lengthofService_name
-}
