62156cae1eee68d60212ba13a0ce27f7cc3ebc64
Sami Islam <islam.samipkr@gmail.com>
2023-12-20 20:08:20 -0500

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -1,6 +1,8 @@
-locals {
 
-  service_name = "Abracadabra"
-  owner        = "Community Team"
-  lengthofService_name=length(local.service_name)
+locals {​
+
+  service_name = "forum"​
+  owner        = "Community Team"​
+  lengthofservice_name=length(local.service_name)
+  lengthofowner_name=length(local.owner)
 }
