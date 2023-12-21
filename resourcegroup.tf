6937bc9b49e441ec680ba3eec22214ceb50c29c4
Sami Islam <islam.samipkr@gmail.com>
2023-12-20 21:00:51 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -5,10 +5,10 @@ resource "azurerm_resource_group" "mcit420zz5um" {
 
 
 resource "azurerm_storage_account" "azurermmcit" {
-  name                     = "${var.myname}storageaccountname${var.countNumber}"
+  name                     = "${var.myname}storage${var.countNumber}"
   resource_group_name      = azurerm_resource_group.mcit420zz5um.name
   location                 = azurerm_resource_group.mcit420zz5um.location
-  account_tier             = "Standard"
+  account_tier             = var.account_tier
   account_replication_type = "GRS"
   access_tier="Cool"
   cross_tenant_replication_enabled = false
