6f5730c145c2e7aec06422ba89f8c3a946326a90
Sami Islam <islam.samipkr@gmail.com>
2023-12-22 17:05:18 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -10,6 +10,14 @@ resource "azurerm_storage_account" "azurermmcit" {
     environment = var.environment
   }
 }
+resource "azurerm_storage_account" "sa" {
+  name                     = "${lower(var.naming_prefix)}${random_integer.sa_num.result}"
+  resource_group_name      = azurerm_resource_group.setup.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
 resource "azurerm_storage_container" "ct" {
   #count                = length(var.component)
   name                 = "terraform-state${var.component}"
