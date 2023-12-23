ccee63cb499b81a662286eba1d412291b0a83430
Ruthkwe08 <146134486+Ruthkwe08@users.noreply.github.com>
2023-12-22 20:04:18 -0500

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -9,4 +9,5 @@ lengthfirstname=length(local.myfirstname)
 lengthlastname=length(local.mylastname)
 bestmealinafrica=["achu","eru","ndole","fufu"]
 canadianfood=["poutine","stew","bannock","lobster","smoked meat","maple syrup","butter tarts","chips","salmon","timbits"]
+numbers=["1","2","3","4","5","6","7","8","9","10"]
 }
