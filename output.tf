e5d183db380b27786cf07a44767b8263ddd929b9
Sami Islam <islam.samipkr@gmail.com>
2023-12-22 20:05:16 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -23,3 +23,7 @@ for 1st element in -->inside the list variable winterlistOfSports--->"icehockey"
 for 2nd element in -->inside the list variable winterlistOfSports--->"snowboarding" 2nd element
 for 3rd element in -->inside the list variable winterlistOfSports--->"iceskating" 3rd element
 */
+
+output "sumofthreenumbers"{
+    value=sum([for numberoutput in local.total_output: numberoutput])
+}
