962211383649a566ace1c92584de1cfa87c46365
Sami Islam <islam.samipkr@gmail.com>
2023-12-22 19:33:43 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -18,4 +18,8 @@ output "countNumber"{
 output "wintersportslist"{
     value=[for sport in local.winterlistOfSports:sport]
 }
-
+/*
+for 1st element in -->inside the list variable winterlistOfSports--->"icehockey"1st element
+for 2nd element in -->inside the list variable winterlistOfSports--->"snowboarding" 2nd element
+for 3rd element in -->inside the list variable winterlistOfSports--->"iceskating" 3rd element
+*/
