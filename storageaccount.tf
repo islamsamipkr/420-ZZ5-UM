8e34cbaf2465aa4518331887a718f7983bce21f2
Ruthkwe08 <146134486+Ruthkwe08@users.noreply.github.com>
2024-01-05 17:23:09 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,1 +1,15 @@
-
+resource "azurerm_resource_group" "mcitresourcegroup"
+{
+name = "mynewresourcegroup"
+location = "Canada Central"
+}
+resource "azurerm_storage_account" "example" {
+name = "storageaccountname"
+resource_group_name = azurerm_resource_group.mcitresourcegroup.name
+location = azurerm_resource_group.mcitresourcegroup.location
+account_tier = "Standard"
+account_replication_type = "GRS"
+tags = {
+environment = "staging"
+}
+}
