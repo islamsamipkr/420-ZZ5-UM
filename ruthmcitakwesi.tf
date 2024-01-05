746b538b626c5d8228a7415e543e5d984159aadd
Ruthkwe08 <146134486+Ruthkwe08@users.noreply.github.com>
2024-01-05 17:44:31 -0500

    Update ruthmcitakwesi.tf

--- ruthmcitakwesi.tf
+++ ruthmcitakwesi.tf
@@ -1,18 +1,18 @@
 variable "storage_account_names" {
   type    = list(string)
-  default = ["JohnMCITLastname1", "JaneMCITLastname1", "AlexMCITLastname1", "EmilyMCITLastname1"]
+  default = ["RuthMCITAkwesi1", "RuthMCITAkwesi2", "RuthMCITAkwesi3", "RuthMCITAkwesi4"]
 }
 
 resource "azurerm_storage_account" "storage_accounts" {
   count                = length(var.storage_account_names)
   name                 = var.storage_account_names[count.index]
   resource_group_name  = azurerm_resource_group.example.name
-  location             = "East US"  # Replace with your desired location
+  location             = "Canada Central"  
   account_tier         = "Standard"
   account_replication  = "LRS"
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resource-group"
-  location = "East US"  # Replace with your desired location
+  location = "Canada Central"  
 }
