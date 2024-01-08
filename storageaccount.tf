ebca2b39b08445a86205e2f9c495a392e75cdf60
Sami Islam <islam.samipkr@gmail.com>
2024-01-08 08:16:20 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -24,3 +24,16 @@ resource "azurerm_storage_container" "ct" {
   storage_account_name = azurerm_storage_account.sa.name
 
 }
+resource "azurerm_storage_account" "azurermmcitcountexample" {
+  count=5
+  name                     = "${var.myname}storage${count.index}"
+  resource_group_name      = azurerm_resource_group.mcit420zz5um.name
+  location                 = azurerm_resource_group.mcit420zz5um.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+  access_tier=var.access_tier
+  cross_tenant_replication_enabled = var.cross_tenant_replication_enabled
+  tags = {
+    environment = var.environment
+  }
+}
