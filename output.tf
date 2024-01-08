6e17c2fbdb74982047fb234cba24782ebefaa623
Sami Islam <islam.samipkr@gmail.com>
2024-01-08 07:55:36 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -27,3 +27,43 @@ for 3rd element in -->inside the list variable winterlistOfSports--->"iceskating
 output "sumofthreenumbers"{
     value=sum([for numberoutput in local.total_output: tonumber(numberoutput)])
 }
+output "id" {
+  value = [
+    for cluster in azurerm_kubernetes_cluster.myk8s: cluster.id
+  ]
+}
+
+output "kube_config" {
+  sensitive = true
+  value = [ 
+    for cluster in azurerm_kubernetes_cluster.myk8s: cluster.kube_config_raw
+  ]
+}
+
+output "client_key" {
+  sensitive = true
+  value = [
+    for cluster in azurerm_kubernetes_cluster.myk8s: cluster.kube_config.0.client_key
+  ]
+}
+
+output "client_certificate" {
+  sensitive = true
+  value = [
+    for cluster in azurerm_kubernetes_cluster.myk8s: cluster.kube_config.0.client_certificate
+  ]
+}
+
+output "cluster_ca_certificate" {
+  sensitive = true
+  value = [
+    for cluster in azurerm_kubernetes_cluster.myk8s: cluster.kube_config.0.cluster_ca_certificate
+  ]
+}
+
+output "host" {
+  sensitive = true
+  value = [
+    for cluster in azurerm_kubernetes_cluster.myk8s: cluster.kube_config.0.host
+  ]
+}
